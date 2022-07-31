e46773a7b7d257f8a31c0ef7174425a6ffef47d7
Aynaz Lotfata (She/Her) <61927496+alotfata@users.noreply.github.com>
2022-08-01 03:47:31 +0530

    Update GGPLOT2_MOVIE.R

--- GGPLOT2_MOVIE.R
+++ GGPLOT2_MOVIE.R
@@ -15,3 +15,31 @@ h+xlab("money axis")+
         legend.position=c(1,1),
         legend.justification=c(1,1),
         plot.title= element_text(color = "DarkGreen", size=30, family="Courier"))
+
+
+#ggplot2 boxplot 
+
+u<- ggplot(data=movies, aes(x=movies$Ratings, y=movies$AudienceRatings, col=Genre))
+u+geom_point()+geom_smooth(fill=NA)
+
+u <- ggplot(data=movies, aes(x=Genre, y= movies$AudienceRatings, colour=Genre))
+u+ geom_boxplot(size=1.2)+geom_point()
+u+ geom_boxplot(size=1.2)+geom_jitter()
+
+
+#ggplot2 layers
+
+v<- ggplot(data=movies, aes(x=movies$Budgetmillion))
+v+geom_histogram(binwidth=10, aes(fill=Genre), color="Blue")
+  
+v+geom_histogram(binwidth=10, aes(fill=Genre), color="Blue") +facet_grid(Genre~., scales="free")
+
+w<- ggplot(data=movies, aes(x=movies$Ratings, y=movies$AudienceRatings, col=Genre))
+w+geom_point(size=3)+facet_grid(Genre~Year)+geom_smooth()
+
+#ggplot2 coordination 
+
+n<-ggplot(data=movies, aes(x=movies$Budgetmillion))
+n+geom_histogram(binwidth = 10, aes(fill=Genre), col="Black")+ylim(0,50)
+n+geom_histogram(binwidth = 10, aes(fill=Genre), col="Black")+coord_cartesian(ylim=c(0,50))
+
